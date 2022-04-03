24396ce8254fac9add4acdd691ff32d809459865
fDll37 <dmendelev@yandex.ru>
2022-04-03 16:44:03 +0300

    final version

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Данил Менделев on 03.04.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+    
+    @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var userName: UILabel!
+    
+    @IBOutlet weak var userBirthday: UILabel!
+    @IBOutlet weak var userCity: UILabel!
+    @IBOutlet weak var userInformation: UITextView!
+}
---
+++ Netology_IB_Instruments/ProfileViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Данил Менделев on 03.04.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let screenWidth = UIScreen.main.bounds.width
+        if let profileView = Bundle.main.loadNibNamed("ProfileView", owner: nil, options: nil)?.first as? ProfileView {
+            profileView.frame = CGRect(x: 16, y: 60, width: screenWidth - 32, height: 500)
+            view.addSubview(profileView)
+        }
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
