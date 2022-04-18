329df0e07939bc5dbc32b9ef67b1e7f065977218
fDll37 <dmendelev@yandex.ru>
2022-04-19 01:09:16 +0300

    make homework Auto Layout

--- Netology_IB_Instruments/FeedViewController.swift
+++ Netology_IB_Instruments/FeedViewController.swift
@@ -15,25 +15,63 @@ struct Post{
 
 class FeedViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .yellow
-        makeButtonViewPost()
-    }
-    
-    let post = Post(title: "Название поста", image: UIImage(named: "A316DE42"), text: "текст внутри данного поста, очень длинный и важный")
-    
-    private func makeButtonViewPost(){
-        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 250, height: 30))
-        button.center = view.center
+    private let firstButton: UIButton = {
+        let button = UIButton()
         button.backgroundColor = .black
         button.setTitle("Посмотреть пост", for: .normal)
-        view.addSubview(button)
-        button.addTarget(self, action: #selector(tapButtonViewPost), for: .touchUpInside)
-    }
-    @objc private func tapButtonViewPost(){
+        button.addTarget(self, action: #selector(tapFirstButtonViewPost), for: .touchUpInside)
+        return button
+        }()
+    
+   @objc private func tapFirstButtonViewPost(){
         let postVC = PostViewController()
         postVC.navigationItem.title = post.title
         navigationController?.pushViewController(postVC, animated: true)
     }
+    
+    private let secondButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .black
+        button.setTitle("Посмотреть пост2", for: .normal)
+        button.addTarget(self, action: #selector(tapSecondButtonViewPost), for: .touchUpInside)
+        return button
+        }()
+    
+   @objc private func tapSecondButtonViewPost(){
+        let postVC = PostViewController()
+        postVC.navigationItem.title = post.title
+        navigationController?.pushViewController(postVC, animated: true)
+    }
+    
+    private let stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.backgroundColor = .brown
+        stackView.axis = .vertical
+        stackView.distribution = .fillProportionally
+        stackView.spacing = 10
+        return stackView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+        layoutStackView()
+    }
+    
+    let post = Post(title: "Название поста", image: UIImage(named: "A316DE42"), text: "текст внутри данного поста, очень длинный и важный")
+    
+
+    private func layoutStackView(){
+        
+        view.addSubview(stackView)
+        [firstButton, secondButton].forEach{stackView.addArrangedSubview($0)}
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+
+
 }
